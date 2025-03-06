bddc3cf45e24943a9a27cdeccf9afe4960bad1d0
septech <septechx@proton.me>
2025-03-06 19:03:51 +0100

    Add assert in case of missing api key

--- apps/web/lib/actions.ts
+++ apps/web/lib/actions.ts
@@ -4,6 +4,8 @@ import type { ShopResponse } from "@fnsn/types";
 
 export async function fetchShop(): Promise<ShopResponse> {
   "use cache";
+  if (!process.env.FN_API_KEY) throw new Error("No api key provided");
+
   const shop = await fetch(
     "https://fortniteapi.io/v2/shop?includeRenderData=false&includeHiddenTabs=false",
     {
