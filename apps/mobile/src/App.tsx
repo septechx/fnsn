183983d83301a8f1fb30cd4855f8ed39ba454bb0
septech <septechx@proton.me>
2025-03-10 19:58:05 +0100

    Initial mobile commit

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+export function App() {
+  return (
+    <view>
+      <text className="text-green-500">Hello world</text>
+    </view>
+  );
+}
---
+++ apps/mobile/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  corePlugins: {
+    textTransform: false,
+  },
+};
